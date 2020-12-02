15d96dd55c797d456a76cd4f6831eb917b81fe15
tee mong <jquilestino16.com>
2020-12-02 14:43:07 +0800

    Initial commit

---
+++ advance.php
@@ -0,0 +1,15 @@
+<?php
+
+$prutas = array("bayabas","tambis","mansanitas","saging");
+$total = count($prutas);
+
+
+$numbers = array(1,4,5,2,6,7,10,9,8);
+sort($numbers);
+
+if(in_array("tambis",$prutas)){
+
+    echo "benta mo";
+}else{
+    echo "uwi ka na";
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+// data types
+$test1 = "pogi ko";
+$num1 = 7;
+$cokefloat = 5;
+$guava = "yellow";
+$size = "small";
+
+$mahiwaga = array($test1,$num1,$cokefloat,);
+
+foreach($mahiwaga as $values){
+    echo $values. "<br/>";
+}
